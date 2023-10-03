00052c504cdf4bff72505933be46005a33758c68
BUNJEE <vsolomatin@kse.org.ua>
2023-10-03 15:59:55 +0300

    upgraded structure, starting work on Assigment#3

--- api_utils.py
+++ api_utils.py
@@ -5,3 +5,31 @@ def fetch_users_last_seen(offset=0):
     response = requests.get(f"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}",
                             headers={'accept': 'application/json'})
     return response
+
+
+def fetch_online_users(offset=0):
+
+    # Define the base URL for the API
+    base_url = "https://sef.podkolzin.consulting/api/stats/users"
+
+    # Define the date parameter
+    date_param = "2023-27-09-20:00"  # Update with your desired date and time
+
+    # Create the full URL with the date parameter
+    url = f"{base_url}?date={date_param}"
+
+    try:
+        # Make the GET request
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse and print the response JSON
+            data = response.json()
+            print("Users Online:", data.get("usersOnline"))
+        else:
+            print("Request failed with status code:", response.status_code)
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
--- main.py
+++ main.py
@@ -2,12 +2,29 @@ from time_utils import humanize_time_difference
 from api_utils import fetch_users_last_seen
 
 
+def feature1(response1_data):
+    pass
+
+
+def feature2(response1_data):
+    pass
+
+
+def feature3(response1_data):
+    pass
+
+
+def feature4(response1_data):
+    pass
+
 
 while True:
     offset = 0
     lang = input("Enter your language, виберіть мову: en, ua: ")
+
     if lang not in ['ua', 'en']:
         lang = "en"
+
 
     while True:
         count = 0
@@ -28,3 +45,27 @@ while True:
         else:
             print(f"Failed to fetch data. Status code: {response.status_code}")
             break
+
+        while True:
+            input_command = input("Input your feature: ")
+
+            if input_command == 1:
+                feature1(response_data)
+            elif input_command == 2:
+                feature2(response_data)
+            elif input_command == 3:
+                feature3(response_data)
+            else:
+                feature4(response_data)
+
+    # while True:
+    #     input_command = input("Input your feature: ")
+    #
+    #     if input_command == 1:
+    #         feature1(response_data)
+    #     elif input_command == 2:
+    #         print("Option 2 selected")
+    #     elif input_command == 3:
+    #         print("Option 3 selected")
+    #     else:
+    #         print("Invalid option")
